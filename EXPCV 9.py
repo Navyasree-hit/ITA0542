06a9d46dca7a4e53f942eead245d929b2b509814
Navyasree-hit <navyasree4359@gmail.com>
2026-05-02 11:42:54 +0530

    Add files via upload

---
+++ EXPCV 9.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+# Read image
+image = cv2.imread("C:/Users/gowri/Desktop/bird.jpeg", cv2.IMREAD_GRAYSCALE)
+
+# Create kernel
+kernel = np.ones((5,5), np.uint8)
+
+# Apply dilation
+dilated_image = cv2.dilate(image, kernel, iterations=1)
+
+# Show output
+cv2.imshow("Original Image", image)
+cv2.imshow("Dilated Image", dilated_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
